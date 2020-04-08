36fd90798f01c56d9d9f1ae0e9e5fb7928cde263
C <chrismatusemail@gmail.com>
2020-04-08 10:49:24 +0100

    double players

--- app.js
+++ app.js
@@ -1,21 +1,106 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
+// const express = require("express");
+// const http = require("http");
+// const socketIo = require("socket.io");
 
+// const port = process.env.PORT || 4001;
+// const index = require("./index.js");
+
+// const app = express();
+// app.use(index);
+
+// const server = http.createServer(app);
+
+// const io = socketIo(server);
+
+// server.listen(port, () => console.log(`Listening on port ${port}`));
+
+//A basic Node.js server that will serve our game file
+
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
 const port = process.env.PORT || 4001;
-const index = require("./index.js");
+//Now let's have the socket object listen to our server object.
+var io = require("socket.io").listen(server);
 
-const app = express();
-app.use(index);
+console.log("SERVER SOCKET");
+console.log(io);
+console.log("SERVER SOCKET");
+//When this project gets more complex, should we export this io above instead of doing global var?
 
-const server = http.createServer(app);
+//Now look, here we're storing our game data on the server. But irl we'd want to database it, then if server failed, we'd still recover state of the game.
+var players = {};
+var star = {
+  x: Math.floor(Math.random() * 700) + 50,
+  y: Math.floor(Math.random() * 500) + 50,
+};
+var scores = {
+  blue: 0,
+  red: 0,
+};
 
-const io = socketIo(server);
+app.use(express.static(__dirname + "/public"));
 
-io.on("connection", (socket) => {
-  console.log("New client connected"),
-    setInterval(() => socket.emit("time", new Date().toTimeString()), 1000);
-  socket.on("disconnect", () => console.log("Client disconnected"));
+//Please serve the index.html file as the root page.
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+//What we want the 'socket object listening to our server object' to do under partic conditions.
+io.on("connection", function (socket) {
+  setInterval(() => socket.emit("time", new Date().toTimeString()), 1000);
+
+  //Look above! This is where 'socket' is created as an argument. It is an incoming socket connexion from a client,
+  //whereas 'io' is the Socket.io server instance, listening to our http server.
+  console.log(`${socket.id} user connected`);
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: Math.floor(Math.random() * 2) == 0 ? "red" : "blue",
+  };
+
+  socket.emit("currentPlayers", players); // send the full players object to just the new player
+
+  socket.emit("starLocation", star);
+  socket.emit("scoreUpdate", scores);
+
+  socket.broadcast.emit("newPlayer", players[socket.id]); // update all other players (but not current) of the new player
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+
+    delete players[socket.id]; // remove this player from our players object
+
+    io.emit("disconnect", socket.id); // emit a message to all players to remove this player
+  });
+
+  // when a player moves, update the player data
+  socket.on("playerMovement", function (movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit("playerMoved", players[socket.id]);
+  });
+
+  socket.on("starCollected", function () {
+    if (players[socket.id].team === "red") {
+      scores.red += 10;
+    } else {
+      scores.blue += 10;
+    }
+    star.x = Math.floor(Math.random() * 700) + 50;
+    star.y = Math.floor(Math.random() * 500) + 50;
+    io.emit("starLocation", star);
+    io.emit("scoreUpdate", scores);
+  });
+});
+
+server.listen(port, function () {
+  console.log(`Listening on ${server.address().port}`);
+});
+
+// console.log(`Listening on port ${port}`)
